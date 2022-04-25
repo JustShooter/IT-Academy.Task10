6b18519cb8d4e79a67e265617bb2fefa34910d81
AleksandrStrezhik <aleksandrstrehzik@gmail.com>
2022-04-25 23:08:00 +0300

    Алексей сказал 2

---
+++ repository/src/test/java/by/it/academy/task10/dao/DaoTest.java
@@ -0,0 +1,103 @@
+package by.it.academy.task10.dao;
+
+import by.it.academy.task10.entity.Student;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DaoTest {
+
+    Dao<Student> studentDao = new Dao<>(Student.class);
+    Student student = Student.builder()
+            .name("Sonia")
+            .surname("Volkova")
+            .role("Student")
+            .build();
+
+    @Test
+    public void create() throws SQLException {
+        Student student1 = studentDao.create(this.student);
+        student1.setName("Victor");
+        Integer id = student1.getId();
+        studentDao.update(student1);
+        Student student2 = studentDao.findOne(id);
+        assertEquals(student.getName(), "Victor");
+    }
+
+    @Test
+    public void findOne() throws SQLException {
+        Student student1 = studentDao.create(Student.builder()
+                .name("Sasha")
+                .surname("Borsik")
+                .role("Student")
+                .build());
+        Integer id = student1.getId();
+        assertEquals(student1, studentDao.findOne(id));
+    }
+
+    @Test
+    public void findAll() throws SQLException {
+        Student student1 = studentDao.create(Student.builder()
+                .name("Maxim")
+                .surname("Maximov")
+                .role("Student")
+                .build());
+        List<Student> all = studentDao.findAll();
+        Student s = all.stream()
+                .filter(st -> st.getName().equals(student1.getName())
+                        && st.getSurname().equals(student1.getSurname()))
+                .findAny().orElse(null);
+        assertNotNull(s);
+    }
+
+    @Test
+    public void update() {
+        Student student1 = studentDao.create(Student.builder()
+                .name("Sergio")
+                .surname("Berluskoni")
+                .role("Student")
+                .build());
+        Student student2 = studentDao.create(student1);
+        student1.setRole("Mentor");
+        studentDao.update(student1);
+        assertEquals(student1.getRole(), student2.getRole());
+    }
+
+    @Test
+    public void delete() throws SQLException {
+        Student student1 = studentDao.create(Student.builder()
+                .name("Vladimir")
+                .surname("Lenin")
+                .role("Admin")
+                .build());
+        Integer id = student1.getId();
+        studentDao.delete(student1);
+        List<Student> all = studentDao.findAll();
+        Student s = all.stream()
+                .filter(st -> st.getName().equals(student1.getName())
+                        && st.getSurname().equals(student1.getSurname()))
+                .findAny().orElse(null);
+        assertNull(s);
+    }
+
+    @Test
+    public void deleteById() throws SQLException {
+        Student student1 = studentDao.create(Student.builder()
+                .name("Nicolai")
+                .surname("Romanov")
+                .role("Admin")
+                .build());
+        Integer id = student1.getId();
+        studentDao.deleteById(id);
+        List<Student> all = studentDao.findAll();
+        Student s = all.stream()
+                .filter(st -> st.getName().equals(student1.getName())
+                        && st.getSurname().equals(student1.getSurname()))
+                .findAny().orElse(null);
+        assertNull(s);
+    }
+}
---
+++ service/src/test/java/by/it/academy/task10/AdminServiceTest.java
@@ -0,0 +1,95 @@
+package by.it.academy.task10;
+
+import by.it.academy.task10.dao.Dao;
+import by.it.academy.task10.entity.Course;
+import by.it.academy.task10.entity.Mentor;
+import by.it.academy.task10.entity.Student;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class AdminServiceTest {
+    AdminService as = new AdminService();
+    GeneralService gs = new GeneralService();
+    private Dao<Student> studentDao = new Dao<>(Student.class);
+    Dao<Mentor> mentorDao = new Dao<>(Mentor.class);
+    Dao<Course> courseDao = new Dao<>(Course.class);
+
+    @Before
+    public void init(){
+        as.createStudent("Сергей", "Сварцевич");
+        as.createStudent("John", "Weak");
+        as.addCourse("Java");
+        as.createMentor("Жанна", "Колесень");
+        as.createMentor("Инга", "Чешун");
+        as.addCourse("C++");
+    }
+
+    @Test
+    public void createStudentSuccessfully() {
+        assertTrue(as.createStudent("Степан", "Смоленский"));
+    }
+
+    @Test
+    public void createStudentFailure() {
+        assertFalse(as.createStudent("Сергей", "Сварцевич"));
+    }
+
+    @Test
+    public void deleteStudent() throws SQLException {
+        as.deleteStudent("John", "Weak");
+        assertNull(gs.getIdUser("John", "Weak"));
+    }
+
+    @Test
+    public void addStudentToCourse() throws SQLException {
+        as.addStudentToCourse("Степан", "Смоленский", "Java");
+        Set<Course> courses = studentDao.findOne
+                (gs.getIdUser("Степан", "Смоленский")).getCourses();
+        assertNotNull(courses);
+    }
+
+    @Test
+    public void createMentorSuccessfully() {
+        assertTrue(as.createMentor("Елизавета", "Велимович"));
+    }
+
+    @Test
+    public void createMentorFailure() {
+        assertFalse(as.createMentor("Жанна", "Колесень"));
+    }
+
+    @Test
+    public void deleteMentor() throws SQLException {
+        as.deleteMentor("Инга", "Чешун");
+        assertNull(gs.getIdUser("Инга", "Чешун"));
+    }
+
+    @Test
+    public void addMentorToCourse() throws SQLException {
+        as.addMentorToCourse("Жанна", "Колесень", "Java");
+       /* Course java = courseDao.findOne(gs.getIdCourse("Java"));*/
+        Course course = mentorDao.findOne
+                (gs.getIdUser("Жанна", "Колесень")).getCourseMentor();
+        /*assertEquals(course, java);*/
+        assertNotNull(course);
+    }
+
+    /*@Test
+    public void getAllCourses() throws SQLException {
+        List<Course> list = as.getAllCourses();
+        Course course = list.get(gs.getIdCourse("Java") - 1);
+        Course one = courseDao.findOne(gs.getIdCourse("Java"));
+        assertEquals(course, one);
+    }*/
+
+    @Test
+    public void addCourseFailure() {
+        assertFalse(as.addCourse("C++"));
+    }
+}
